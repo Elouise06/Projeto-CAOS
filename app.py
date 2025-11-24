428e55367ee55c4624166a3662136e2c21eff920
Erick Wellington <160539840+erickwelli@users.noreply.github.com>
2025-11-24 14:13:12 +0000

    novas rotas criadas e utilizando laços de repetição e decisão

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 
+livros = []
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -30,16 +32,32 @@ def cadastro():
 def login():
     return render_template('login.html')
 
+@app.route('/cadastrarlivro')
+def cadastrarlivro():
+    return render_template('cadastrar_livro.html')
+
 @app.route('/verlivro', methods=['POST'])
 def verlivro():
     nome = request.form['nome']
     autor = request.form['autor']
-    valor = int(request.form['valor'])
+    valor = float(request.form['valor'])
     if valor <= 30:
         mensagem = 'Esse livro está apto para as vendas!'
     else:
         mensagem = 'Esse livro não está apto para as vendas!'
-    return render_template('livro_cadastrado.html', mensagem = mensagem, autor = autor, nome = nome)
+
+    livro = {
+        'nome': nome,
+        'autor': autor,
+        'valor': valor,
+        'mensagem': mensagem
+    }
+    livros.append(livro)
+    return render_template('livro_cadastrado.html',livro = livro)
+
+@app.route('/listalivros')
+def listalivros():
+    return render_template('lista_livro.html', livros=livros)
 
 if __name__ == "__main__":
     app.run()
